58ec297ca283ff0ba32d3c0fd6c0a1d14eedf824
mart-ski <mskidev@gmail.com>
2024-12-01 13:07:56 +0100

    feat(ingrédient): Création model Ingredient

---
+++ src/Domain/MenuZen.Domain/Models/Ingrédient.cs
@@ -0,0 +1,22 @@
+﻿using MartSki.Framework.Domain.ResultPattern.Models;
+
+namespace MenuZen.Domain.Models;
+
+public class Ingrédient
+{
+    public string Nom { get; init; }
+    public string? Icône { get; init; }
+    public CatégorieIngrédients CatégorieIngrédients { get; init; }
+
+    private Ingrédient(string nom, string? icône, CatégorieIngrédients catégorieIngrédients)
+    {
+        Nom = nom;
+        Icône = icône;
+        CatégorieIngrédients = catégorieIngrédients;
+    }
+
+    public static Result<Ingrédient> Create(string nom, string? icône, CatégorieIngrédients catégorieIngrédients)
+    {
+        return Result<Ingrédient>.Success(new Ingrédient(nom, icône, catégorieIngrédients));
+    }
+}
