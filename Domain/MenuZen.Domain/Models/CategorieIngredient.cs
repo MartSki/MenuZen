260cfb842c89694aa9cfff05f080b37d904db34b
mart-ski <mskidev@gmail.com>
2024-12-01 10:32:21 +0100

    feat: Création model CategorieIngredient

---
+++ Domain/MenuZen.Domain/Models/CategorieIngredient.cs
@@ -0,0 +1,20 @@
+﻿namespace MenuZen.Domain.Models;
+
+public class CategorieIngredient
+{
+    public string Nom { get; init; }
+    public string? Icone { get; init; }
+    public string? Couleur { get; init; }
+
+    private CategorieIngredient(string nom, string? icone, string? couleur)
+    {
+        Nom = nom;
+        Icone = icone;
+        Couleur = couleur;
+    }
+
+    public static CategorieIngredient Create(string nom, string? icone, string? couleur)
+    {
+        return new CategorieIngredient(nom, icone, couleur);
+    }
+}
